538c4b4856dca50581b79fa320bbb9fe428e4ac3
Sean <sean.jennings1@gmail.com>
2022-02-18 16:46:46 -0300

    1_money_change_again dp solution.

--- algorithmic_toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ algorithmic_toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -1,10 +1,25 @@
 # Uses python3
 import sys
+import math
+
+DENOMS = [1, 3, 4]
+COMPUTED = {}
 
 def get_change(m):
-    #write your code here
-    return m // 4
+    if m in COMPUTED:
+        return COMPUTED[m]
+    if m == 0:
+        COMPUTED[m] = 0
+        return 0
+    if m < 0:
+        COMPUTED[m] = math.inf
+        return math.inf
+    by_denom = [get_change(m - d) + 1 for d in DENOMS]
+    best = min(by_denom)
+    COMPUTED[m] = best
+    return best
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
     print(get_change(m))
+    # print('computed:', COMPUTED)
