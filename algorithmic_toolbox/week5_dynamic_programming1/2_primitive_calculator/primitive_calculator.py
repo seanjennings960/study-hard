36d8e6da84c3b6c8b5cb8ac40ba2b0697e7bac55
Sean <sean.jennings1@gmail.com>
2022-02-19 17:04:19 -0300

    Solve primitive calculator problem.

--- algorithmic_toolbox/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
+++ algorithmic_toolbox/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
@@ -1,16 +1,35 @@
 # Uses python3
+import math
 import sys
 
+def best_previous(min_ops, i):
+    div3 = min_ops[i // 3] if i % 3 == 0 else math.inf
+    div2 = min_ops[i // 2] if i % 2 == 0 else math.inf
+    add = min_ops[i - 1]
+
+    previous = [i // 3, i // 2, i - 1]
+    prev_ops = [div3, div2, add]
+    best = prev_ops.index(min(prev_ops))
+    return previous[best]
+
 def optimal_sequence(n):
-    sequence = []
-    while n >= 1:
-        sequence.append(n)
-        if n % 3 == 0:
-            n = n // 3
-        elif n % 2 == 0:
-            n = n // 2
+    min_ops = {}
+    for i in range(1, n + 1):
+        if i == 1:
+            ops = 0
         else:
-            n = n - 1
+            best = best_previous(min_ops, i)
+            ops = min_ops[best] + 1
+        min_ops[i] = ops
+    return backtrace(min_ops, n)
+
+def backtrace(min_ops, n):
+    sequence = []
+    x = n
+    while x > 1:
+        sequence.append(x)
+        x = best_previous(min_ops, x)
+    sequence.append(1)
     return reversed(sequence)
 
 input = sys.stdin.read()
