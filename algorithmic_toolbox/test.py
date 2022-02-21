c47b3bed19f39e450d4cb71400209d94e5784a49
Sean <sean.jennings1@gmail.com>
2022-02-21 12:34:29 -0300

    Support both Rust and python files in test script.

--- algorithmic_toolbox/test.py
+++ algorithmic_toolbox/test.py
@@ -1,3 +1,4 @@
+import os
 from typing import Dict, Tuple
 from subprocess import Popen, PIPE
 from pathlib import Path
@@ -5,7 +6,25 @@ import yaml
 import click
 
 
-def parse_dir(test_dir: Path) -> Tuple[Dict, Path]:
+def verify_unique(files, test_dir, msg):
+    if not files:
+        raise click.UsageError(f'Could not find {msg} in dir {test_dir}')
+    if len(files) != 1:
+        raise click.UsageError(f'Found multiple {msg}s in dir {test_dir}.')
+
+def detect_py(test_dir):
+    files = [f for f in test_dir.iterdir() if f.is_file()]
+    files = list(filter(lambda f: f.suffix == '.py', files))
+    verify_unique(files, test_dir, 'python files')
+    return files[0]
+
+def detect_rs(test_dir):
+    dirs = [d for d in test_dir.iterdir() if d.is_dir()]
+    dirs = list(filter(lambda d: (d / 'Cargo.toml').exists(), dirs))
+    verify_unique(dirs, test_dir, 'rust project')
+    return dirs[0]
+
+def parse_dir(test_dir: Path, lang: str) -> Tuple[Dict, Path]:
     # Load solutions from yaml
     sol_file =test_dir / 'solutions.yaml'
     if not (sol_file).exists():
@@ -14,24 +33,36 @@ def parse_dir(test_dir: Path) -> Tuple[Dict, Path]:
     with open(sol_file, 'rb') as f:
         solutions = yaml.load(f, Loader=yaml.loader.BaseLoader)
 
+    if lang == 'py':
+        code = detect_py(test_dir)
+    elif lang == 'rs':
+        code = detect_rs(test_dir)
+
     # Autoparse for python file.
-    files = [f for f in test_dir.iterdir() if f.is_file()]
-    files = list(filter(lambda f: f.suffix == '.py', files))
-    if not files:
-        raise click.UsageError(f'Could not find python file in dir {test_dir}')
-    if len(files) != 1:
-        raise click.UsageError(f'Found multiple python files in dir {test_dir}.')
-    return solutions, files[0]
+    return solutions, code
+
+def run_code(code, lang):
+    kwargs = {'stdin': PIPE, 'stdout': PIPE}
+    if lang == 'py':
+        return Popen(['python3', code], **kwargs)
+    elif lang == 'rs':
+        return Popen(['cargo', 'run'], **kwargs)
+    raise ValueError
+
 
 @click.command()
 @click.argument('test-dir', type=click.Path(exists=True),
                 required=True)
-def main(test_dir):
+@click.option('--lang', type=click.Choice(['py', 'rs']), default='py')
+def main(test_dir, lang):
     test_dir = Path(test_dir)
-    solutions, code = parse_dir(test_dir)
+    solutions, code = parse_dir(test_dir, lang)
+
+    if lang == 'rs':
+        os.chdir(code)
 
     for i, case in enumerate(solutions['test_cases']):
-        p = Popen(['python3', code], stdin=PIPE, stdout=PIPE)
+        p = run_code(code, lang)
         p.stdin.write(case['in'].encode())
         p.stdin.close()
         computed = b''
