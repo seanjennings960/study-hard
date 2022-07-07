4c66038aa128cd2649638c30e80f4d38643dba5f
Sean <sean.jennings1@gmail.com>
2022-07-07 18:23:31 -0500

    Add data_structures course.

---
+++ data_structures/week1_basic_data_structures/1_brackets_in_code/check_brackets.py
@@ -0,0 +1,48 @@
+# python3
+
+from collections import namedtuple
+
+Bracket = namedtuple("Bracket", ["char", "position"])
+
+
+def are_matching(left, right):
+    return (left + right) in ["()", "[]", "{}"]
+
+
+def find_mismatch(text):
+    stack = []
+    for i, char in enumerate(text):
+        if char in "([{":
+            stack.append(Bracket(char, i))
+
+
+        if char in ")]}":
+            if not stack:
+                return i
+            top = stack.pop().char
+            if ((top == '(' and char != ')') or
+                    (top == '[' and char != ']') or
+                    (top == '{' and char != '}')):
+                return i
+
+    first = None
+    while stack:
+        first = stack.pop()
+    if first is not None:
+        return first.position
+    return first
+
+
+
+def main():
+    text = input()
+    mismatch = find_mismatch(text)
+    if mismatch is None:
+        print('Success')
+    else:
+        print(mismatch + 1)
+    # Printing answer, write your code here
+
+
+if __name__ == "__main__":
+    main()
