f1ee978c9a820a2538979e5a51267740bd6aa18d
Sean <sean.jennings1@gmail.com>
2023-09-20 22:11:25 -0600

    Finish up 4th problem for systems hw.

--- controls/linear_systems/hw1/hw1.jl
+++ controls/linear_systems/hw1/hw1.jl
@@ -1,3 +1,4 @@
+using LinearAlgebra
 A = [
     -1 1 0 0
     0.5 -1 0.5 0
@@ -28,12 +29,48 @@ function change_basis(A, p_b2_b1)
 
 end
 
+function x_at_1(eAt, integral_part)
+    x0 = [1; 0]
+    B = [1; 0]
+    return eAt * (x0 + integral_part * B)
+end
+
 function harmonic()
+    eAt = [
+        cos(1) sin(1)
+        -sin(1) cos(1)
+    ]
     println("part a")
+    A = [0 1; -1 0]
+    conv_a = A^-1 * (I - eAt^-1)
+    println("by hand")
     display(
         [cos(1) + sin(1)
-         cos(1) + sin(1) - 1]
+         cos(1) - sin(1) - 1]
     )
+    println("computer")
+    display(x_at_1(eAt, conv_a))
+    # println("conv_a")
+    # display(conv_a)
+
+    println("part b")
+    
+    conv_b = A^-1 * (I + A^-1) * (I - eAt^-1)
+    a = [cos(1); -sin(1)]
+    b = [sin(1) - (cos(1) - 1); (cos(1) - 1) + sin(1)]
+    println("by hand")
+    display(
+        a + b
+    )
+    println("computer")
+    display(x_at_1(eAt, conv_b))
+
+    println("part c")
+    conv_c = 1/4 * [
+        1 - cos(2) sin(2) - 2
+        2 - sin(2) 1 - cos(2)
+    ]
+    display(x_at_1(eAt, conv_c))
 end
 
 
