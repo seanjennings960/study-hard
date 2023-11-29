44786ad24cca2b203e3c1ac75dadc3ecd39d9527
Sean <sean.jennings1@gmail.com>
2023-11-29 10:53:50 -0700

    Remove project2.

--- renewable_energy/project2/optimal_design/__init__.py
+++
@@ -1,7 +0,0 @@
-from .single_period import SinglePeriodModel, SinglePeriodResult
-from .multi_period import MultiPeriodModel, MultiPeriodResult
-
-__all__ = [
-    'SinglePeriodModel', 'SinglePeriodResult',
-    'MultiPeriodModel', 'MultiPeriodResult'
-]
