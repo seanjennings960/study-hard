d2258b6d045c43cc337caaf6c53b7e77e67d3fb7
Sean <sean.jennings1@gmail.com>
2024-02-18 13:50:21 -0700

    Finish HW2.

--- nonlinear_systems/hw2/problems.py
+++ nonlinear_systems/hw2/problems.py
@@ -3,6 +3,26 @@ import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 
 
+def phase_space(dynamics, x1_range, x2_range, x0s=None, t_span=None):
+
+    x_1, x_2 = np.meshgrid(x1_range, x2_range)
+    x_dot = dynamics(None, np.dstack([x_1, x_2]))
+    x_1_dot = x_dot[..., 0]
+    x_2_dot = x_dot[..., 1]
+    
+
+    fig = plt.figure()
+    mag = np.linalg.norm(np.dstack([x_1_dot, x_2_dot]), axis=-1)
+    plt.quiver(x_1, x_2, x_1_dot, x_2_dot, mag, angles='xy', minshaft=0, minlength=1)
+
+    if x0s is not None:
+        assert t_span is not None
+        x_solutions = [solve_ivp(dynamics, t_span, x0, atol=1e-8, rtol=1e-10) for x0 in x0s]
+        for i, traj in enumerate(x_solutions):
+            plt.plot(traj.y[0, 0], traj.y[1,0], marker='o', linestyle='', color=f"C{i}", label=f"x0 = {x0s[i]}")
+            plt.plot(traj.y[0], traj.y[1], color=f"C{i}", label=f"Trajectory {i}")
+        plt.legend()
+    return fig
 
 def problem4():
     a = 2/3
@@ -11,28 +31,12 @@ def problem4():
 
     x1_range = np.linspace(0, 3, 20)
     x2_range = np.linspace(0, 2, 20)
-    x_1, x_2 = np.meshgrid(x1_range, x2_range)
 
-    # def dynamics(x_1, x_2):
-    #     return (
-    #         (a - b * x_2) * x_1,
-    #         (c * x_1 - d) * x_2
-    #     )
-    # dyn_vec = np.vectorize(dynamics, otypes=[float, float], signature='(n),(n)->(n),(n)')
     def dynamics(t, x):
         # (..., 2)
         x1_dot = (a - b * x[..., 1]) * x[..., 0]
         x2_dot = (c * x[..., 0] - d) * x[..., 1]
         return np.concatenate([x1_dot[..., np.newaxis], x2_dot[..., np.newaxis]], axis=-1)
-
-    x_dot = dynamics(None, np.dstack([x_1, x_2]))
-    x_1_dot = x_dot[..., 0]
-    x_2_dot = x_dot[..., 1]
-
-    print('x_1_dot')
-    print(x_1_dot)
-    print('x_2_dot')
-    print(x_2_dot)
 
     x0s = np.array([
         [1, 1],
@@ -40,19 +44,14 @@ def problem4():
         [0.25, 1.2]
     ])
     t_span = [0, 20]
-    x_solutions = [solve_ivp(dynamics, t_span, x0, atol=1e-8, rtol=1e-10) for x0 in x0s]
+
+    fig = phase_space(dynamics, x1_range, x2_range, x0s, t_span)
+    plt.title("Predator/prey interactions")
+    plt.xlabel("x1")
+    plt.ylabel("x2")
+    fig.savefig("Images/pred-prey.png")
 
 
-
-
-
-    plt.figure()
-    mag = np.linalg.norm(np.dstack([x_1_dot, x_2_dot]), axis=-1)
-    plt.quiver(x_1, x_2, x_1_dot, x_2_dot, mag, angles='xy', minshaft=0, minlength=1)
-    for i, traj in enumerate(x_solutions):
-        plt.plot(traj.y[0, 0], traj.y[1,0], marker='o', linestyle='', color=f"C{i}", label=f"x0 = {x0s[i]}")
-        plt.plot(traj.y[0], traj.y[1], color=f"C{i}", label=f"Trajectory {i}")
-    plt.legend()
     plt.show()
 
 
@@ -90,12 +89,8 @@ def problem5():
         y_dot = r_dot * np.sin(theta) + r * np.cos(theta) * theta_dot
         return np.concatenate([x_dot[..., np.newaxis], y_dot[..., np.newaxis]], axis=-1)
 
-    x1_range = np.linspace(-2, 2, 31)
-    x2_range = np.linspace(-2, 2, 31)
-    x_1, x_2 = np.meshgrid(x1_range, x2_range)
-    x_dot = dynamics(None, np.dstack([x_1, x_2]))
-    x_1_dot = x_dot[..., 0]
-    x_2_dot = x_dot[..., 1]
+    x1_range = 0.1* np.linspace(-2, 2, 31)
+    x2_range = 0.1* np.linspace(-2, 2, 31)
 
     x0s = np.array([
         [1, 1],
@@ -107,16 +102,7 @@ def problem5():
         [-2, -2]
     ])
     t_span = [0, 20]
-    x_solutions = [solve_ivp(dynamics, t_span, x0, atol=1e-8, rtol=1e-10) for x0 in x0s]
-
-    plt.figure()
-    for i, traj in enumerate(x_solutions):
-        plt.plot(traj.y[0, 0], traj.y[1,0], marker='o', linestyle='', color=f"C{i}", label=f"x0 = {x0s[i]}")
-        plt.plot(traj.y[0], traj.y[1], color=f"C{i}", label=f"Trajectory {i}")
-
-    mag = np.linalg.norm(np.dstack([x_1_dot, x_2_dot]), axis=-1)
-    plt.quiver(x_1, x_2, x_1_dot, x_2_dot, mag, angles='xy', minshaft=0, minlength=1)
-    plt.legend()
+    phase_space(dynamics, x1_range, x2_range, x0s=x0s, t_span=t_span)
     plt.show()
 
     
@@ -128,7 +114,7 @@ def problem5():
 
 
 def main():
-    problem5()
+    problem4()
 
 
 if __name__ == '__main__':
